cfeb78ac6cb60b28eeb3ad0f1dbdec81f5081c80
Pushpendra081 <pushpendra1.mdl@gmail.com>
2025-10-04 20:56:25 +0530

    Added the Contact Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { FaReact } from "react-icons/fa";
 import Navbar from './components/navbar';
 import Herosection from './components/herosection.jsx'
-// import Contact from './pages/contact.jsx'
+import Contact from './pages/contact.jsx'
 import Footer from './components/footer.jsx'
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
     <div className="bg-[#090040] ">
       <Navbar />
       <Herosection/>
-      {/* <Contact/> */}
+      <Contact/>
       <Footer/>
     </div >
   )
---
+++ src/pages/contact.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { MdEmail } from "react-icons/md";
+
+const Contact = () => {
+    return (
+        <div class=" justify-items-center mt-[4px] px-[128px] py-[80px]  rounded-[10px] ">
+            <div className="w-full max-w-[520px] my-[28px] px-[45px] py-[28px] bg-[#fff] rounded-[10px] flex flex-col justify-center items-center">
+                <div>
+                    <h1 class="text-[38px] text-[#090040] font-semibold tracking-normal">Let's Connect</h1>
+                </div>
+                <div class="w-full max-w-[450px] mt-[28px]  rounded-[10px]">
+                    <form action="" class=" flex flex-col gap-[10px]">
+
+                        {/* Name */}
+                        <label htmlFor="" class="mb-[-4px] text-[#090040]">Name</label>
+                        <input type="text" name="name" title="Enter your name here!" class="w-full px-[16px] py-[10px] border-2 border-[#d2b4e7] focus:border-[#471396] rounded-[10px] focus:outline-none transition-all duration-300 focus:bg-[#e4e0e8]" placeholder="Enter your name here!" required />
+
+                        {/* Email */}
+                        <label htmlFor="" class="mb-[-4px] text-[#090040]">Email</label>
+                        <input type="email" name="email" title="Enter your email here!" class="w-full px-[16px] py-[10px] border-2 border-[#d2b4e7] focus:border-[#471396] rounded-[10px] focus:outline-none transition-all duration-300 focus:bg-[#e4e0e8]" placeholder="Enter your email here!" required />
+
+
+                        {/* Message */}
+                        <label htmlFor="" class="mb-[-4px] text-[#090040]">Message</label>
+                        <textarea title="Tell me how can i help you?" name="message" maxlength="500" placeholder="Hi, let's connect!" class="w-full h-[160px] px-[16px] py-[10px] border-2 border-[#d2b4e7] focus:border-[#471396] rounded-[10px] focus:outline-none transition-all duration-300 focus:bg-[#e4e0e8]" required></textarea>
+
+
+                        {/* Send Email */}
+                        <span className="flex flex-row justify-end items-center ">
+
+                            <a title="Send email via google!" type="submit" class=" flex flex-row items-center mt-[6px] gap-[14px] text-[18px] font-medium border-2 border-[#d2b4e7] px-[16px] py-[10px] rounded-[10px] transition-colors duration-300 cursor-pointer  hover:bg-[#d2b4e7] ">
+                                Send Email
+                                <MdEmail size={22} />
+                            </a>
+
+                        </span>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Contact
